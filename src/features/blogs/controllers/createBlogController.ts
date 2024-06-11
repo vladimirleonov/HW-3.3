2b8ce445497e488f7ed5b16f7b5fd1614ab6a7e0
vladimir <vladimirleonov789@gmail.com>
2024-06-11 14:05:36 +0300

    refactor createBlogController

--- src/features/blogs/controllers/createBlogController.ts
+++ src/features/blogs/controllers/createBlogController.ts
@@ -5,13 +5,19 @@ import {blogService} from "../services/blogService"
 import {blogMongoQueryRepository} from "../repository/blogMongoQueryRepository"
 import {Result} from "../../../common/types/result"
 
-export const createBlogController = async (req: Request<{}, BlogOutputType, BlogBodyInputType>, res: Response<BlogOutputType>) => {
+export const createBlogController = async (req: Request<{}, BlogOutputType, BlogBodyInputType>, res: Response<BlogOutputType | string>) => {
     try {
         const result: Result<string> = await blogService.createBlog(req.body)
 
         const blog: BlogOutputType | null = await blogMongoQueryRepository.findForOutputById(result.data)
 
         //? !
+        if(!blog) {
+            //error if just created blog not found
+            res.status(HTTP_CODES.INTERNAL_SERVER_ERROR).send('something went wrong');
+            return;
+        }
+
         res.status(HTTP_CODES.CREATED).send(blog!)
 
     } catch (err) {
